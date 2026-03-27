acf690910cb18adb5169f6eafe4a91e8b14a0b03
Jindarat <jindarat132522@gmail.com>
2026-03-27 22:12:56 +0700

    prepare for render

--- app.py
+++ app.py
@@ -385,4 +385,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=10000)
